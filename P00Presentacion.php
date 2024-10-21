e27d7ce111815573a805e4095249ace823e6282d
Angel Briceño <angel.briceno.campos@cbtis89.edu.mx>
2024-10-21 09:30:54 -0600

    Add files via upload

---
+++ P00Presentacion.php
@@ -0,0 +1,15 @@
+<?php
+/* CBTIS 89
+P00Presentaciòn
+Programa que contiene mis datos personales
+Angel Gabriel Briceño Campos
+3ºA Programaciòn */
+echo "<b>PRESENTACION</b>","<br>","<br>";
+echo "Mi nombre es: Angel Gabriel Briceño Campos";
+echo"<br>";
+echo "Estudio en el CBTIS 89";
+echo "<br>";
+echo "En tercer semestre de la carrera de programaciòn A matutino";
+echo "<br>";
+echo "Vivo en la ciudad de Durango,Dgo. Mexico";
+?>
