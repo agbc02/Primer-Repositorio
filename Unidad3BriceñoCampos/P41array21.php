b7e943d2c4fec1669e5b8a6a8e147b1048d12fd9
Angel Briceño <angel.briceno.campos@cbtis89.edu.mx>
2024-11-11 10:55:10 -0600

    Add files via upload

--- Unidad3BriceñoCampos/P41array21.php
+++ Unidad3BriceñoCampos/P41array21.php
@@ -1,4 +1,16 @@
 <?php
+/* CBTIS 89
+P41array21.php
+Programa que almacena en un arreglo llamado $Compras el valor de 6 compras, posteriormente en otros arreglos se debe almacenar informacion de acuerdo a las siguientes condiciones:
+El contenido del arreglo $Descuentos se llenarà de la siguiente manera:
+Compra menor 300 = 0
+Compra mayor 300 y menor o igual 1000 = 10%
+Compra mayor 1000 y menor o igual 2500 = 10%
+Compra mayor 2500 y menor o igual 5000 = 10%
+compra mayor a 5000 = 25%
+Briceño Campos Angel Gabriel
+Programacion 3ºA T.M
+*/
 $Compras = array(800,150,2700,1900,7600);
 $Descuentos = array();
 $Total = array();
@@ -32,7 +44,7 @@ for($i=0;$i<$longitud;$i++){
 			$Porcentaje[]="25%";
 		}
 	}
-	echo "Compras-- "," Descuentos-- "," Total-- "," Porcentaje","<br>";
+	echo "Compras-- "," Descuentos---- "," Total-- "," Porcentaje","<br>";
 	for($j=0;$j<$longitud;$j++)
 		echo "---".$Compras[$j]."------------".$Descuentos[$j]."------------".$Total[$j]."------------".$Porcentaje[$j]."<br>";
 ?>
